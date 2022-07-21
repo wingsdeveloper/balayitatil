1c8a03c693ae79a7afce2a8f451277206f20970a
cumalikaya <cali.kaya3@gmail.com>
2022-07-21 20:56:34 +0300

    card 2

--- resources/views/villa/reservation/index.blade.php
+++ resources/views/villa/reservation/index.blade.php
@@ -648,7 +648,7 @@
                                </label>
                            </div>
                            <div class="Rez-left-payment-item-right">
-                               <input type="text" name="number" id="creditcard" class="form-control" placeholder="0000-0000-0000-0000"/>
+                               <input type="text" name="number" id="creditcard" class="form-control" data-inputmask="'mask': '9999 9999 9999 9999'" placeholder="0000-0000-0000-0000"/>
                            </div>
                         </div>
                         <div class="Rez-left-payment-item">
