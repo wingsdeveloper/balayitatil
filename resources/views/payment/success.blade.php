24e20f7831ce9059c377d24a1da3a4f6ca12cd40
sadettinwings <sadettin@wings.com.tr>
2022-07-28 12:23:19 +0300

    rezervasyon onay

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -7,6 +7,64 @@
 
 @endsection
 @section('content')
+@php
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.infoset.app/v1/deals?Name='. $code .'&additionalProp1[op]=0&additionalProp1[val]=string&additionalProp2[op]=0&additionalProp2[val]=string&additionalProp3[op]=0&additionalProp3[val]=string');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+
+$headers = array();
+$headers[] = 'Accept: application/json';
+$headers[] = 'X-Api-Key: 418a190c-ebae-4d1c-bee3-cf9395b141ee';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+$result = json_decode($result);
+$rezinfoset = ($result->items)[0]->id;
+$cusinfoset = ($result->items)[0]->contactId;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+
+curl_close($ch);
+
+// echo json_encode($result);
+
+//error_reporting(E_ALL);
+
+//echo $rezinfoset;
+
+// Ödeme alındı bilgisi gönderiyoruz 
+
+$postData=[ "notes" =>"success"];  
+               
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://api.infoset.app/v1/deals/'. $rezinfoset .'');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+
+
+$headers = array();
+$headers[] = 'Accept: application/json';
+$headers[] = 'X-Api-Key: 418a190c-ebae-4d1c-bee3-cf9395b141ee';
+$headers[] = 'Content-Type: application/json-patch+json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+   echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+// echo json_encode($result);
+
+
+@endphp
 <section class="Rez-finish ">
         <div class="Rez-finish-ok flex-column a-i-c" style="width: 100%">
             <svg class='icon icon-yes'><use xlink:href='#icon-yes'></use></svg>
